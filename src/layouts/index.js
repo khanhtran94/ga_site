9b3e95f88f3f094bb572fc50a05e02d6217e1474
khanhTran <khanhtrantung190894@gmail.com>
2020-06-20 12:40:12 +0700

    file system

---
+++ src/layouts/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default ({ children }) =>
+  <div>
+    Layout <br />
+    {children}
+    Layout <br />
+  </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,25 @@
 import React from "react"
-import { Link } from "gatsby"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
-
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
+export default ({ data }) => (
+  <div style={{ color: 'tomato' }}>
+    <h1>Hello word</h1>
+    <p>This is a paragraph</p>
+    <h2>{data.site.siteMetadata.title}</h2>
+    <h2>{data.site.siteMetadata.author}</h2>
+  </div>
 )
 
-export default IndexPage
+export const query = graphql`
+  query FirstQuery {
+  allFile {
+    edges {
+      node {
+        relativePath
+        extension
+        prettySize
+      }
+    }
+  }
+}
+
+`
